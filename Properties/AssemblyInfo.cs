20cc4ec5c1ef8e45075e78f03c1f3e5a694a090a
Michael Evenson <michael.evenson@flexusergroup.com>
2025-11-14 09:14:37 -0500

    Add files via upload

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -22,6 +22,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("abbf6de1-a117-4904-9c8c-3887fcabf038")]
 
-[assembly: AssemblyVersion("1.0.5.8")]
+[assembly: AssemblyVersion("1.0.5.9")]
 
-//[assembly: AssemblyFileVersion("1.0.4.12")]
+//[assembly: AssemblyFileVersion("1.0.4.13")]
